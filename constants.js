07aa61abae3e550dea47201cb8c5098e2b110a80
Nicolas Agustin <nicolastrres@gmail.com>
2019-01-19 17:28:01 -0300

    Add mongo service to platformsh

--- constants.js
+++ constants.js
@@ -1,5 +1,28 @@
-const DB_URL = 'mongodb://localhost:27017/featuresDB'
-const DB_NAME = 'featuresDB'
+const { head } = require('lodash')
+
+
+const getDbUrl = () => {
+  if (process.env.PLATFORMSH_ENV) {
+    const config = require('platformsh').config()
+    const db = head(config.relationships.database)
+    return `${db['scheme']}://${db['username']}:${db['password']}@${db['ip']}:${db['port']}/${db['path']}`
+  }
+
+  return 'mongodb://localhost:27017/featuresDB'
+}
+
+const getDbName = () => {
+  if (process.env.PLATFORMSH_ENV) {
+    const config = require('platformsh').config()
+    return head(config.relationships.database).path
+  }
+  return 'featuresDB'
+}
+
+
+const DB_URL = getDbUrl()
+const DB_NAME = getDbName()
+
 
 
 module.exports = { DB_URL, DB_NAME }
