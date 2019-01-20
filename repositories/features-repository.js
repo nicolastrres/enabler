8d75223c61747c6c47142a36eaae003902f0b1c8
Nicolas Agustin <nicolastrres@gmail.com>
2019-01-19 21:47:27 -0300

    Creates features in repository

--- repositories/features-repository.js
+++ repositories/features-repository.js
@@ -1,9 +1,14 @@
 const databaseClient = require('../database/client')
+const { get, omit, map } = require('lodash/fp')
 
 const getAllFeatures = () => databaseClient.getAll('features')
 
 const getFeature = (featureName) => databaseClient.get('features', { name: featureName })
 
-const createFeatures = () => {}
+const createFeatures = (features) => {
+  return databaseClient.create('features', features)
+    .then(get('ops'))
+    .then(map(omit(['_id'])))
+}
 
 module.exports = { createFeatures, getAllFeatures, getFeature }
--- tests/repositories/features-repository.test.js
+++ tests/repositories/features-repository.test.js
@@ -3,18 +3,51 @@ const { stub } = require('sinon')
 const databaseClient = require('../../database/client')
 const featuresRepository = require('../../repositories/features-repository')
 
-
 describe('Features repository test', () => {
   before(() => {
-    stub(databaseClient, 'getAll').returns([{ name: 'feature1', enabled: false }])
-    stub(databaseClient, 'get').returns({ name: 'feature1', enabled: false })
+    stub(databaseClient, 'getAll')
+      .withArgs('features')
+      .resolves([{ name: 'feature1', enabled: false }])
+
+    stub(databaseClient, 'get')
+      .withArgs('features', { name: 'feature1' })
+      .resolves({ name: 'feature1', enabled: false })
+
+    stub(databaseClient, 'create').resolves({
+      result: { ok: 1, n: 2 },
+      ops: [
+        { name: 'feature1', enabled: false, _id: '0' },
+        { name: 'feature2', enabled: true, _id: '1' }
+      ]
+    })
+  })
+
+  after(() => {
+    databaseClient.getAll.restore()
+    databaseClient.get.restore()
+    databaseClient.create.restore()
   })
 
   it('gets all features', async () => {
-    expect(await featuresRepository.getAllFeatures()).to.be.deep.equal([{ name: 'feature1', enabled: false }])
+    const response = await featuresRepository.getAllFeatures()
+
+    expect(response).to.be.deep.equal([{ name: 'feature1', enabled: false }])
   })
 
   it('gets feature', async () => {
-    expect(await featuresRepository.getFeature()).to.be.deep.equal({ name: 'feature1', enabled: false })
+    const response = await featuresRepository.getFeature('feature1')
+
+    expect(response).to.be.deep.equal({ name: 'feature1', enabled: false })
+  })
+
+  it('creates feature', async () => {
+    const features = [
+      { name: 'feature1', enabled: false },
+      { name: 'feature2', enabled: true }
+    ]
+
+    const response = await featuresRepository.createFeatures(features)
+
+    expect(response).to.be.deep.equal(features)
   })
 })
